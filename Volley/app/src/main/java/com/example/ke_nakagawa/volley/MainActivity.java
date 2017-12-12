4ba46fb91d92b91731aeb4cf9031bfbfab7efdd3
nkws <keigo_nakagawa@voyagegroup.com>
2017-12-12 23:48:12 +0900

    add Volley, and implement http request

---
+++ Volley/app/src/main/java/com/example/ke_nakagawa/volley/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.ke_nakagawa.volley;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView mTextView = (TextView) findViewById(R.id.testText);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://www.google.com";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                mTextView.setText("Response is:" + response.substring(0, 500));
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+
+        queue.add(stringRequest);
+    }
+}
